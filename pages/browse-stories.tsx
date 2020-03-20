ba2e278797e4c1b87e92c4bf991467357360b463
sakulstra <lukasstrassel@googlemail.com>
2020-03-20 22:05:12 +0100

    fix: setup module aliases

--- pages/browse-stories.tsx
+++ pages/browse-stories.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { Story } from "@ty";
+
+const stories: Story[] = [
+  { title: "bla", parts: ["A long time ago"], createdAt: new Date() }
+];
 
 export default function BrowseStories() {
   return <div>browse existing stories</div>;
---
+++ src/types/story.ts
@@ -0,0 +1,5 @@
+export type Story = {
+  title: string;
+  parts: string[];
+  createdAt: Date;
+};
