cb0b8e345df319ec241ae285e2610966e2b46385
Lukas Strassel <lukasstrassel@googlemail.com>
2020-03-21 11:18:32 +0100

    feat: add authentication (#1)

--- pages/write-a-story.tsx
+++ pages/write-a-story.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import LoginUi from "@components/LoginUi";
 
 export default function WriteAStory() {
-  return <div>Start a new story</div>;
+  return (
+    <div>
+      Start a new story
+      <LoginUi />
+    </div>
+  );
 }
