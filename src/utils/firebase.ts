47fdbcd12f4a0a5edac63b29f8def6aec532362c
sakulstra <lukasstrassel@googlemail.com>
2020-03-22 00:42:41 +0100

    feat: some more form improvements & starting to save data in the db

---
+++ src/utils/firebase.ts
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+// Configure Firebase.
+const config = {
+  apiKey: "AIzaSyB9M0rMoZtJ0VHyksZO9DGtXBaXcH7E8TI",
+  authDomain: "corona-stories.firebaseapp.com",
+  databaseURL: "https://corona-stories.firebaseio.com",
+  projectId: "corona-stories",
+  storageBucket: "corona-stories.appspot.com",
+  messagingSenderId: "579433547134",
+  appId: "1:579433547134:web:7542cf74ae12c7dbe1df2f",
+  measurementId: "G-29E766Z3ZV"
+};
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(config);
+}
+
+export default firebase;
