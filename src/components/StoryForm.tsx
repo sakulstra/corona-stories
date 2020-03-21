d27abf8ca75ae21c22f4f5d5b0436a64afda5174
sakulstra <lukasstrassel@googlemail.com>
2020-03-21 23:44:23 +0100

    feat: setup initial form

--- src/components/StoryForm.tsx
+++ src/components/StoryForm.tsx
@@ -1,33 +1,66 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import ImageSelector from "@components/ImageSelector";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
+  root: {
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+    flexDirection: "column",
+    maxWidth: 400,
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1)
+    }
+  },
+  img: {
+    maxWidth: 400,
+    margin: theme.spacing(1)
+  },
   helperText: {
     textAlign: "right"
   }
-});
+}));
 
 export default function CustomTextField(props) {
   const classes = useStyles();
-  const [value, setValue] = useState("");
-  function handleChange(e) {
-    setValue(e.target.value);
-  }
-  const currentValue = props.value || value;
-  const maxLength = props.maxLength || 400;
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+  const [image, setImage] = useState("");
+  const maxLength = 400;
   return (
-    <TextField
-      {...props}
-      fullWidth
-      value={currentValue}
-      onChange={props.onChange || handleChange}
-      multiline
-      inputProps={{ maxLength }}
-      FormHelperTextProps={{ className: classes.helperText }}
-      helperText={`${currentValue.length}/${maxLength}`}
-      variant="outlined"
-      rows={6}
-    />
+    <div className={classes.root}>
+      <img
+        className={classes.img}
+        src={
+          image ||
+          "https://api.deepai.org/job-view-file/cec969fc-4866-406c-bcaf-0d5dacb4f6ae/outputs/output.png"
+        }
+      />
+      <ImageSelector keyword={title} onChange={setImage} />
+      <TextField
+        fullWidth
+        variant="outlined"
+        label="Title"
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+      />
+      <TextField
+        {...props}
+        label="The story must go on"
+        fullWidth
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+        multiline
+        inputProps={{ maxLength }}
+        FormHelperTextProps={{ className: classes.helperText }}
+        helperText={`${message.length}/${maxLength}`}
+        variant="outlined"
+        rows={6}
+      />
+      <Button variant="outlined">Start your story</Button>
+    </div>
   );
 }
