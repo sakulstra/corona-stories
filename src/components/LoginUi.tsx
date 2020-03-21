0466f42039783f79ced142e8fc0d4d4ef87b2e3b
sakulstra <lukasstrassel@googlemail.com>
2020-03-21 12:10:55 +0100

    refactor: enhance login ui

--- src/components/LoginUi.tsx
+++ src/components/LoginUi.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import firebase from "firebase";
 
@@ -22,7 +22,10 @@ const uiConfig = {
   // Popup signin flow rather than redirect flow.
   signInFlow: "popup",
   // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: "/signedIn",
+  callbacks: {
+    // Avoid redirects after sign-in.
+    signInSuccessWithAuthResult: () => false
+  },
   // We will display Google and Facebook as auth providers.
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
@@ -31,7 +34,19 @@ const uiConfig = {
 };
 
 export default function SignInScreen() {
+  const [user, setUser] = useState<any>(null);
+  useEffect(() => {
+    return firebase.auth().onAuthStateChanged(user => setUser(user));
+  }, []);
   return (
-    <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    <>
+      Hallo, {user && firebase.auth().currentUser.displayName}!
+      {!user && (
+        <StyledFirebaseAuth
+          uiConfig={uiConfig}
+          firebaseAuth={firebase.auth()}
+        />
+      )}
+    </>
   );
 }
