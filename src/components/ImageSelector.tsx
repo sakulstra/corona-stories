95df2d2b700a7d8681788d5c4580484d5130e88b
Jonas Strassel <jonas.strassel@mobilehead.com>
2020-03-22 12:38:33 +0100

    feature: extract keywords

--- src/components/ImageSelector.tsx
+++ src/components/ImageSelector.tsx
@@ -1,103 +1,74 @@
-import React, { useState, useEffect } from "react";
-import deepai from "deepai";
-import Unsplash from "unsplash-js";
-import firebase from "../utils/firebase";
+import React, { useState, useEffect } from 'react'
+import deepai from 'deepai'
+import Unsplash from 'unsplash-js'
+//import firebase from '../utils/firebase'
+import {
+    UploadMetadata,
+    RandomPhotoResponse,
+    MetSearchResponse,
+    MetSingleResponse,
+    DeepResponse,
+    UnsplashSingleItem,
+} from 'src/types'
+import keywordExtractor from 'keyword-extractor'
+import LanguageDetect from 'languagedetect'
+import { StripChar } from 'stripchar'
 
 const unsplash = new Unsplash({
-  accessKey: "955c92676fbff5987db2c89ebad2118cbd67b7dfbf424263454ab60d029dcef1"
-});
+    accessKey:
+        '955c92676fbff5987db2c89ebad2118cbd67b7dfbf424263454ab60d029dcef1',
+})
 
-deepai.setApiKey("b1f6a192-c7fc-4ca2-917d-80e92cb1cc4f");
+deepai.setApiKey('b1f6a192-c7fc-4ca2-917d-80e92cb1cc4f')
 
-type MetSearchResponse = {
-  total: number;
-  objectIDs: number[];
-};
-
-type MetSingleResponse = {
-  title: string;
-  artistDisplayName: string;
-  primaryImage: string;
-  primaryImageSmall: string;
-  objectURL: string;
-};
-
-type DeepResponse = {
-  id: string;
-  output_url: string;
-};
-
-type UnsplashSingleItem = {
-  description: string;
-  user: {
-    name: string;
-  };
-  urls: {
-    raw: string;
-    full: string;
-    regular: string;
-    small: string;
-    thumb: string;
-    toBeUsed?: string;
-  };
-  links: {
-    html: string;
-  };
-};
-
-type UploadMetadata = {
-  photoAuthor: string;
-  photoUrl: string;
-  styleAuthor: string;
-  styleUrl: string;
-  url?: string;
-};
-
-type RandomPhotoResponse = {
-  photo?: UnsplashSingleItem;
-  style?: MetSingleResponse;
-  upload?: UploadMetadata;
-};
+const lngDetector = new LanguageDetect()
 
 const getMetObectIdsForKeyword = (
-  keyword: string
-): Promise<MetSearchResponse> =>
-  fetch(
-    `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${keyword}&hasImages=true&medium=Paintings`
-  ).then(res => res.json());
-
+    keyword?: string
+): Promise<MetSearchResponse> => {
+    let url
+    if (keyword) {
+        url = `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${keyword}&hasImages=true&medium=Paintings`
+    } else {
+        url = `https://collectionapi.metmuseum.org/public/collection/v1/search?q=*&hasImages=true&medium=Paintings&isHighlight=true`
+    }
+    return fetch(url).then((res) => res.json())
+}
 const getSingleMetImageForId = (id: number): Promise<MetSingleResponse> =>
-  fetch(
-    `https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`
-  ).then(res => res.json());
+    fetch(
+        `https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`
+    ).then((res) => res.json())
 
 const randomIntMax = (max: number) => {
-  return Math.floor(Math.random() * (max + 1));
-};
+    return Math.floor(Math.random() * (max + 1))
+}
 
 const getFastStyleTransfer = async (args: RandomPhotoResponse) => {
-  const response: DeepResponse = await deepai.callStandardApi(
-    "fast-style-transfer",
-    {
-      content: `${args.photo.urls.toBeUsed}`,
-      style: args.style.primaryImageSmall
+    const response: DeepResponse = await deepai.callStandardApi(
+        'fast-style-transfer',
+        {
+            content: `${args.photo.urls.toBeUsed}`,
+            style: args.style.primaryImageSmall,
+        }
+    )
+    return {
+        photoAuthor: args.photo.user.name,
+        photoUrl: args.photo.links.html,
+        styleAuthor: args.style.artistDisplayName,
+        styleUrl: args.style.objectURL,
+        url: response.output_url,
     }
-  );
-  return {
-    photoAuthor: args.photo.user.name,
-    photoUrl: args.photo.links.html,
-    styleAuthor: args.style.artistDisplayName,
-    styleUrl: args.style.objectURL,
-    url: response.output_url
-  };
-};
+}
 
 const getRandomMetImage = async (keyword: string) => {
-  const { objectIDs } = await getMetObectIdsForKeyword(keyword);
-  return getSingleMetImageForId(objectIDs[randomIntMax(objectIDs.length)]).then(
-    res => res
-  );
-};
+    let { objectIDs } = await getMetObectIdsForKeyword(keyword)
+    if (!objectIDs || !(objectIDs.length == 0)) {
+        objectIDs = (await getMetObectIdsForKeyword()).objectIDs
+    }
+    return getSingleMetImageForId(
+        objectIDs[randomIntMax(objectIDs.length)]
+    ).then((res) => res)
+}
 
 /*const uploadImage = async (photoInformation: RandomPhotoResponse) => {
   const imageBlob = await fetch(photoInformation.deep.output_url).then(response => response.blob())
@@ -114,37 +85,71 @@ const getRandomMetImage = async (keyword: string) => {
   }
 }*/
 
-const getRandomImage = async (keyword: string): Promise<UploadMetadata> => {
-  const result: RandomPhotoResponse = {};
-  const metImage = getRandomMetImage(keyword).then(
-    metImage => (result.style = metImage)
-  );
-  const unsplashImage = unsplash.photos
-    .getRandomPhoto({ count: 1 })
-    .then(async res => ((await res.json()) as UnsplashSingleItem[])[0])
-    .then(unsplashImage => (result.photo = unsplashImage));
-  await Promise.all([metImage, unsplashImage]);
-  result.photo.urls.toBeUsed = `${result.photo.urls.raw}&fit=crop&w=512&h=512&q=80`;
-  return getFastStyleTransfer(result);
-};
+const getRandomImage = async (keywords: string): Promise<UploadMetadata> => {
+    const result: RandomPhotoResponse = {}
+    const metImage = getRandomMetImage(keywords).then(
+        (metImage) => (result.style = metImage)
+    )
+    const unsplashImage = unsplash.photos
+        .getRandomPhoto({ count: 1 })
+        .then(async (res) => ((await res.json()) as UnsplashSingleItem[])[0])
+        .then((unsplashImage) => (result.photo = unsplashImage))
+    await Promise.all([metImage, unsplashImage])
+    result.photo.urls.toBeUsed = `${result.photo.urls.raw}&fit=crop&w=512&h=512&q=80`
+    return getFastStyleTransfer(result)
+}
 
 export default function ImageSelector({
-  keyword,
-  onChange
+    sentence,
+    onChange,
 }: {
-  keyword: string;
-  onChange: Function;
+    sentence: string
+    onChange: Function
 }) {
-  const getImages = () => {
-    const handler = setTimeout(async () => {
-      const image = await getRandomImage(keyword);
-      console.log(image);
-      onChange(image.url);
-    }, 500);
-    return () => clearTimeout(handler);
-  };
-  useEffect(() => {
-    return getImages();
-  }, [keyword]);
-  return null;
+    const getImages = () => {
+        if (sentence != '') {
+            const availableKeywordlangs = [
+                'danish',
+                'dutch',
+                'english',
+                'french',
+                'galician',
+                'german',
+                'italian',
+                'polish',
+                'portuguese',
+                'romanian',
+                'russian',
+                'spanish',
+                'swedish',
+            ]
+            const handler = setTimeout(async () => {
+                const strippedKeywords = StripChar.RSExceptAlpha(sentence, '_')
+                    .toString()
+                    .replace('_', ' ')
+                const language = lngDetector
+                    .detect(strippedKeywords)
+                    .filter((language) =>
+                        availableKeywordlangs.includes(language[0])
+                    )[0][0]
+                console.log(language)
+                const keywords = keywordExtractor.extract(sentence, {
+                    language,
+                    remove_digits: true,
+                    return_changed_case: true,
+                    remove_duplicates: true,
+                })
+                console.log(keywords)
+                const image = await getRandomImage(
+                    keywords.length > 0 ? keywords : sentence
+                )
+                onChange(image)
+            }, 500)
+            return () => clearTimeout(handler)
+        }
+    }
+    useEffect(() => {
+        return getImages()
+    }, [sentence])
+    return null
 }
--- src/components/StoryForm.tsx
+++ src/components/StoryForm.tsx
@@ -1,84 +1,84 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import ImageSelector from "@components/ImageSelector";
-import firebase from "@utils/firebase";
-import { Story } from "@ty";
-import { useUser } from "@utils/actions/useUser";
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import ImageSelector from '@components/ImageSelector'
+import firebase from '@utils/firebase'
+import { Story, UploadMetadata } from '@ty'
+import { useUser } from '@utils/actions/useUser'
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    alignItems: "center",
-    justifyContent: "center",
-    display: "flex",
-    flexDirection: "column",
-    maxWidth: 400,
-    "& .MuiTextField-root": {
-      margin: theme.spacing(1)
-    }
-  },
-  img: {
-    maxWidth: 400,
-    margin: theme.spacing(1)
-  },
-  helperText: {
-    textAlign: "right"
-  }
-}));
+const useStyles = makeStyles((theme) => ({
+    root: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        maxWidth: 400,
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+        },
+    },
+    img: {
+        maxWidth: 400,
+        margin: theme.spacing(1),
+    },
+    helperText: {
+        textAlign: 'right',
+    },
+}))
 
 export default function CustomTextField(props) {
-  const classes = useStyles();
-  const { user } = useUser();
-  const [title, setTitle] = useState("");
-  const [message, setMessage] = useState("");
-  const [image, setImage] = useState("");
-  const maxLength = 400;
-  const saveStory = () => {
-    const db = firebase.firestore();
-    db.collection("stories")
-      .doc(title)
-      .set({
-        title: title,
-        slug: title,
-        imgSrc: image,
-        parts: [{ text: message, userId: user.uid }],
-        createdAt: firebase.firestore.FieldValue.serverTimestamp()
-      } as Story);
-  };
-  return (
-    <div className={classes.root}>
-      <img
-        className={classes.img}
-        src={
-          image ||
-          "https://api.deepai.org/job-view-file/cec969fc-4866-406c-bcaf-0d5dacb4f6ae/outputs/output.png"
-        }
-      />
-      <ImageSelector keyword={title} onChange={setImage} />
-      <TextField
-        fullWidth
-        variant="outlined"
-        label="Title"
-        value={title}
-        onChange={e => setTitle(e.target.value)}
-      />
-      <TextField
-        {...props}
-        label="The story must go on"
-        fullWidth
-        value={message}
-        onChange={e => setMessage(e.target.value)}
-        multiline
-        inputProps={{ maxLength }}
-        FormHelperTextProps={{ className: classes.helperText }}
-        helperText={`${message.length}/${maxLength}`}
-        variant="outlined"
-        rows={6}
-      />
-      <Button variant="outlined" onClick={saveStory}>
-        Start your story
-      </Button>
-    </div>
-  );
+    const classes = useStyles()
+    const { user } = useUser()
+    const [title, setTitle] = useState('')
+    const [message, setMessage] = useState('')
+    const [image, setImage] = useState({} as UploadMetadata)
+    const maxLength = 400
+    const saveStory = () => {
+        const db = firebase.firestore()
+        db.collection('stories')
+            .doc(title)
+            .set({
+                title: title,
+                slug: title,
+                image,
+                parts: [{ text: message, userId: user.uid }],
+                createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            } as Story)
+    }
+    return (
+        <div className={classes.root}>
+            <img
+                className={classes.img}
+                src={
+                    image.url ||
+                    'https://api.deepai.org/job-view-file/cec969fc-4866-406c-bcaf-0d5dacb4f6ae/outputs/output.png'
+                }
+            />
+            <ImageSelector sentence={title} onChange={setImage} />
+            <TextField
+                fullWidth
+                variant="outlined"
+                label="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+            <TextField
+                {...props}
+                label="The story must go on"
+                fullWidth
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                multiline
+                inputProps={{ maxLength }}
+                FormHelperTextProps={{ className: classes.helperText }}
+                helperText={`${message.length}/${maxLength}`}
+                variant="outlined"
+                rows={6}
+            />
+            <Button variant="outlined" onClick={saveStory}>
+                Start your story
+            </Button>
+        </div>
+    )
 }
