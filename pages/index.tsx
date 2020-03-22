1daa04dc2b3862bc4431eca202c7e3290c6dd560
sakulstra <lukasstrassel@googlemail.com>
2020-03-22 23:14:45 +0100

    feat: improve landing texts

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,25 +15,50 @@ export default function Home() {
             }}
         >
             <Typography variant="h3">
-                {light ? 'Welcome to Corona Stories' : 'fuck you.'}
+                {light
+                    ? 'Welcome to Corona Stories'
+                    : 'Hmpf..., you made it here? Bravo.'}
             </Typography>
 
             <Typography variant="h5" align="center">
                 Unlike the name suggests this is not about the virus corona, but
                 its inevitable effects on our society.
                 <br />
-                With corona speading fast and people slowly following the
-                suggestions to stay at home, there's a gigantic amount of -
-                ungifted, but willing - people sitting at home suddenly finding
-                there alleged creative streak.
                 <br />
-                To prevent this written outpourings from ever wasting paper in
-                print, we've built this platform to help you realize that 1)
-                your in fact not gifted 2) reassure you you're not alone.
+                {light ? (
+                    <>
+                        As people are mostly sitting at home nowadays we created
+                        a collaborative game for all groups of age. The rules
+                        are simple. You can start writing a story and others
+                        will continue and/or you can continue a story someone
+                        else started.
+                    </>
+                ) : (
+                    <>
+                        With corona speading fast and people slowly following
+                        the suggestions to stay at home, there's a gigantic
+                        amount of - ungifted, but willing - people, like you,
+                        sitting at home suddenly finding their alleged "creative
+                        streak".
+                    </>
+                )}
+                <br />
+                <br />
+                {light ? (
+                    <>We're curious to see what you come up with!</>
+                ) : (
+                    <>
+                        To prevent this written outpourings from ever wasting
+                        paper in print, we've built this platform to help you
+                        realize that your in fact not gifted reassure you you're
+                        not alone.
+                    </>
+                )}
+                <br />
             </Typography>
 
             <Grid container>
-                <Grid item xs={6}>
+                <Grid item xs={12} md={6}>
                     <LinkCard passHref href="/write-a-story">
                         <Typography variant="h6">
                             Start your story &rarr;
@@ -45,7 +70,7 @@ export default function Home() {
                         </Typography>
                     </LinkCard>
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item xs={12} md={6}>
                     <LinkCard passHref href="/browse-stories">
                         <Typography variant="h6">
                             Continue a story &rarr;
