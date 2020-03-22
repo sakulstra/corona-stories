3664c7ef824914856856b2a9c4d4bee2aab6f7ec
sakulstra <lukasstrassel@googlemail.com>
2020-03-22 13:17:58 +0100

    fix: add node-fetch import

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,12 @@
-import * as functions from "firebase-functions";
-import admin from "firebase-admin";
+import * as functions from 'firebase-functions'
+import admin from 'firebase-admin'
+import fetch from 'node-fetch'
 
-admin.initializeApp();
+admin.initializeApp()
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+    response.send('Hello from Firebase!')
+})
 /*export const generateSlug = functions.firestore
   .document("stories/{storyId}")
   .onCreate((snap, context) => {
@@ -18,30 +19,27 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   });*/
 
 // in order to maintain user privacy, but still be somehow personal we're generating avatars from https://www.thispersondoesnotexist.com/
-export const profilePicture = functions.auth.user().onCreate(user => {
-  const url = `avatars/${user.uid}.jpg`;
-  const file = admin
-    .storage()
-    .bucket()
-    .file(url);
-  fetch("https://www.thispersondoesnotexist.com/image").then((res: any) => {
-    const contentType = res.headers.get("content-type");
-    const writeStream = file.createWriteStream({
-      metadata: {
-        contentType,
-        metadata: {
-          myValue: 123
-        }
-      }
-    });
-    res.body.pipe(writeStream);
-  });
+export const profilePicture = functions.auth.user().onCreate((user) => {
+    const url = `avatars/${user.uid}.jpg`
+    const file = admin.storage().bucket().file(url)
+    fetch('https://www.thispersondoesnotexist.com/image').then((res: any) => {
+        const contentType = res.headers.get('content-type')
+        const writeStream = file.createWriteStream({
+            metadata: {
+                contentType,
+                metadata: {
+                    myValue: 123,
+                },
+            },
+        })
+        res.body.pipe(writeStream)
+    })
 
-  admin
-    .firestore()
-    .collection("users")
-    .doc(user.uid)
-    .set({
-      profileImg: `https://firebasestorage.googleapis.com/v0/b/corona-stories.appspot.com/o/${url}`
-    });
-});
+    admin
+        .firestore()
+        .collection('users')
+        .doc(user.uid)
+        .set({
+            profileImg: `https://firebasestorage.googleapis.com/v0/b/corona-stories.appspot.com/o/${url}`,
+        })
+})
