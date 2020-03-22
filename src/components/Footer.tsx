3dc69ba2a1d8b6e641239edc48d8923bf0fcb57b
Jonas Strassel <jonas.strassel@mobilehead.com>
2020-03-22 20:22:48 +0100

    fix: other title and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,44 +1,53 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import LightSwitch from "@components/LightSwitch";
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import LightSwitch from '@components/LightSwitch'
 
 const useStyles = makeStyles({
-  root: {
-    marginTop: "20px",
-    position: "relative",
-    width: "100%",
-    height: 100,
-    borderTop: "1px solid #eaeaea",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  twitter: {
-    textDecoration: "none",
-    color: "inherit"
-  },
-  switch: {
-    position: "absolute",
-    right: 0
-  }
-});
+    root: {
+        marginTop: '20px',
+        position: 'relative',
+        width: '100%',
+        height: 100,
+        borderTop: '1px solid #eaeaea',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    social: {
+        textDecoration: 'none',
+        color: 'inherit',
+    },
+    switch: {
+        position: 'absolute',
+        right: 0,
+    },
+})
 
 export default function Footer() {
-  const classes = useStyles();
-  return (
-    <footer className={classes.root}>
-      Proudly presented by&nbsp;
-      <a
-        className={classes.twitter}
-        href="https://twitter.com/sakulstra"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        @sakulstra
-      </a>
-      <div className={classes.switch}>
-        <LightSwitch />
-      </div>
-    </footer>
-  );
+    const classes = useStyles()
+    return (
+        <footer className={classes.root}>
+            Proudly presented by&nbsp;
+            <a
+                className={classes.social}
+                href="https://twitter.com/sakulstra"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                @sakulstra
+            </a>
+            <p>&nbsp;and&nbsp;</p>
+            <a
+                className={classes.social}
+                href="https://github.com/boredland"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                boredland
+            </a>
+            <div className={classes.switch}>
+                <LightSwitch />
+            </div>
+        </footer>
+    )
 }
