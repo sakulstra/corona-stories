f61125c7934d735692ea7df9d9ded3ab4042a1d7
sakulstra <lukasstrassel@googlemail.com>
2020-03-22 12:59:48 +0100

    fix: make it build again

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,8 @@
 import * as functions from "firebase-functions";
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+import admin from "firebase-admin";
+
+admin.initializeApp();
+
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
@@ -15,3 +16,32 @@ export const helloWorld = functions.https.onRequest((request, response) => {
       slug: slugify(value.title)
     });
   });*/
+
+// in order to maintain user privacy, but still be somehow personal we're generating avatars from https://www.thispersondoesnotexist.com/
+export const profilePicture = functions.auth.user().onCreate(user => {
+  const url = `avatars/${user.uid}.jpg`;
+  const file = admin
+    .storage()
+    .bucket()
+    .file(url);
+  fetch("https://www.thispersondoesnotexist.com/image").then((res: any) => {
+    const contentType = res.headers.get("content-type");
+    const writeStream = file.createWriteStream({
+      metadata: {
+        contentType,
+        metadata: {
+          myValue: 123
+        }
+      }
+    });
+    res.body.pipe(writeStream);
+  });
+
+  admin
+    .firestore()
+    .collection("users")
+    .doc(user.uid)
+    .set({
+      profileImg: `https://firebasestorage.googleapis.com/v0/b/corona-stories.appspot.com/o/${url}`
+    });
+});
--- next.config.js
+++ next.config.js
@@ -1,6 +1,40 @@
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
-module.exports = {
+const withAssetRelocator = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      const { isServer } = options;
+
+      if (isServer) {
+        config.node = Object.assign({}, config.node, {
+          __dirname: false,
+          __filename: false
+        });
+
+        config.module.rules.unshift({
+          test: /\.(m?js|node)$/,
+          parser: { amd: false },
+          use: {
+            loader: "@zeit/webpack-asset-relocator-loader",
+            options: {
+              outputAssetBase: "assets",
+              existingAssetNames: [],
+              wrapperCompatibility: true,
+              escapeNonAnalyzableRequires: true
+            }
+          }
+        });
+      }
+
+      if (typeof nextConfig.webpack === "function") {
+        return nextConfig.webpack(config, options);
+      }
+      return config;
+    }
+  });
+};
+
+module.exports = withAssetRelocator({
   reactStrictMode: true,
   webpack: (config, options) => {
     if (config.resolve.plugins) {
@@ -12,4 +46,4 @@ module.exports = {
     return config;
   },
   target: "serverless"
-};
+});
