d3fe1ab47f35abff68b3da431dd3aa4d22588573
Lukas Strassel <lukasstrassel@googlemail.com>
2020-03-23 22:25:01 +0100

    fix: remove custom loader for aliases (#6)
    * fix: remove custom loader for aliases
    
    * fix: update to canary

--- next.config.js
+++ next.config.js
@@ -1,49 +1,41 @@
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-
 const withAssetRelocator = (nextConfig = {}) => {
-  return Object.assign({}, nextConfig, {
-    webpack(config, options) {
-      const { isServer } = options;
+    return Object.assign({}, nextConfig, {
+        webpack(config, options) {
+            const { isServer } = options
 
-      if (isServer) {
-        config.node = Object.assign({}, config.node, {
-          __dirname: false,
-          __filename: false
-        });
+            if (isServer) {
+                config.node = Object.assign({}, config.node, {
+                    __dirname: false,
+                    __filename: false,
+                })
 
-        config.module.rules.unshift({
-          test: /\.(m?js|node)$/,
-          parser: { amd: false },
-          use: {
-            loader: "@zeit/webpack-asset-relocator-loader",
-            options: {
-              outputAssetBase: "assets",
-              existingAssetNames: [],
-              wrapperCompatibility: true,
-              escapeNonAnalyzableRequires: true
+                config.module.rules.unshift({
+                    test: /\.(m?js|node)$/,
+                    parser: { amd: false },
+                    use: {
+                        loader: '@zeit/webpack-asset-relocator-loader',
+                        options: {
+                            outputAssetBase: 'assets',
+                            existingAssetNames: [],
+                            wrapperCompatibility: true,
+                            escapeNonAnalyzableRequires: true,
+                        },
+                    },
+                })
             }
-          }
-        });
-      }
 
-      if (typeof nextConfig.webpack === "function") {
-        return nextConfig.webpack(config, options);
-      }
-      return config;
-    }
-  });
-};
+            if (typeof nextConfig.webpack === 'function') {
+                return nextConfig.webpack(config, options)
+            }
+            return config
+        },
+    })
+}
 
 module.exports = withAssetRelocator({
-  reactStrictMode: true,
-  webpack: (config, options) => {
-    if (config.resolve.plugins) {
-      config.resolve.plugins.push(new TsconfigPathsPlugin());
-    } else {
-      config.resolve.plugins = [new TsconfigPathsPlugin()];
-    }
-
-    return config;
-  },
-  target: "serverless"
-});
+    reactStrictMode: true,
+    experimental: {
+        jsconfigPaths: true, // enables it for both jsconfig.json and tsconfig.json
+    },
+    target: 'serverless',
+})
